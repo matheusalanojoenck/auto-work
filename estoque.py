88fcd05ded20d2b05f06069786e426a773120f00
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2024-02-01 11:51:02 -0300

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -1,4 +1,6 @@
 import csv
+from openpyxl import load_workbook
+
 def main():
     paths = "C:\\Users\\Nota Fiscal\\Downloads\\"
     estoque = ["produtos.csv", "produtos (1).csv"]
@@ -15,7 +17,10 @@ def main():
             for row in reader:
                 item = row["Referencia"]
                 qtd = row["Estoque"]
-                itens_estoque[i][item] = qtd
+                if item in itens_estoque[i]:
+                    itens_estoque[i][item] += qtd
+                else:
+                    itens_estoque[i][item] = qtd
 
     # lendo itens recebidos
     for i in range(len(recebidos)):
@@ -26,12 +31,49 @@ def main():
 
             for row in reader:
                 item = row["Cod. Forn."]
-                qtd = row["Qtde"]
-                itens_recebidos[i][item] = qtd
+                qtd = int(row["Qtde"])
+                if item in itens_recebidos[i]:
+                    itens_recebidos[i][item] += qtd
+                else:
+                    itens_recebidos[i][item] = qtd
 
-    print(itens_estoque)
-    print(itens_recebidos)
+    wb = load_workbook(filename="\\\\MARCELO2018\\Documents\\Faturamento\\Estoque PESO - Template.xlsx")
+    #wb = load_workbook(filename="C:\\Users\\Nota Fiscal\\Documents\\GitHub\\auto-work\\excel\\Estoque PESO.xlsx")
 
+    index = 2
+    for item in itens_estoque[0]:
+        wb["MAV - Estq"][f"A{index}"].value = f'=LEFT(B{index},SEARCH("-",B{index},1)-1)'
+        wb["MAV - Estq"][f"B{index}"].value = item
+        wb["MAV - Estq"][f"C{index}"].value = itens_estoque[0][item]
+        wb["MAV - Estq"][f"D{index}"].value = f"=C{index}/VLOOKUP(A{index},POP!A:C,3,FALSE)"
+        index += 1
+
+    index = 2
+    for item in itens_estoque[1]:
+        wb["IZA - Estq"][f"A{index}"].value = f'=LEFT(B{index},SEARCH("-",B{index},1)-1)'
+        wb["IZA - Estq"][f"B{index}"].value = item
+        wb["IZA - Estq"][f"C{index}"].value = itens_estoque[1][item]
+        wb["IZA - Estq"][f"D{index}"].value = f"=C{index}/VLOOKUP(A{index},POP!A:C,3,FALSE)"
+        index += 1
+
+    index = 2
+    for item in itens_recebidos[0]:
+        wb["MAV - Recb"][f"A{index}"].value = f'=LEFT(B{index},SEARCH("-",B{index},1)-1)'
+        wb["MAV - Recb"][f"B{index}"].value = item
+        wb["MAV - Recb"][f"C{index}"].value = itens_recebidos[0][item]
+        wb["MAV - Recb"][f"D{index}"].value = f"=C{index}/VLOOKUP(A{index},POP!A:C,3,FALSE)"
+        index += 1
+
+    index = 2
+    for item in itens_recebidos[1]:
+        wb["IZA - Recb"][f"A{index}"].value = f'=LEFT(B{index},SEARCH("-",B{index},1)-1)'
+        wb["IZA - Recb"][f"B{index}"].value = item
+        wb["IZA - Recb"][f"C{index}"].value = itens_recebidos[1][item]
+        wb["IZA - Recb"][f"D{index}"].value = f"=C{index}/VLOOKUP(A{index},POP!A:C,3,FALSE)"
+        index += 1
+
+    wb.save("\\\\MARCELO2018\\Documents\\Faturamento\\Estoque PESO.xlsx")
+    #wb.save("C:\\Users\\Nota Fiscal\\Documents\\GitHub\\auto-work\\excel\\Estoque PESO 1.xlsx")
 
 if __name__ == "__main__":
     main()
